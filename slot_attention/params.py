ed1d35b586b5093ee5edbe15373763c11ed5a854
Sahishnu <9358407+sahishnu@users.noreply.github.com>
2023-04-23 14:56:19 -0400

    Update params.py

--- slot_attention/params.py
+++ slot_attention/params.py
@@ -10,7 +10,7 @@ class SlotAttentionParams:
     batch_size: int = 64
     val_batch_size: int = 64
     resolution: Tuple[int, int] = (128, 128)
-    num_slots: int = 5
+    num_slots: int = 7
     num_iterations: int = 3
     data_root: str = "/mnt/data/CLEVR_v1.0/"
     gpus: int = 1
